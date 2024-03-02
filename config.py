f600057fc9cba39864796ef408da1379a7d1ce3a
IDGS-803-21000011 <paulovaladez813@gmail.com>
2024-03-01 18:48:41 -0600

    PEVG 01/03/24 Metodo GET

---
+++ config.py
@@ -0,0 +1,10 @@
+class DevelompentConfig():
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'paulo'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'api_utl_idgs803'
+
+config = {
+    'development': DevelompentConfig
+}
